530d401c04f555b6f1fcdf56a3c97e9765eb7e4f
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-10-11 12:48:00 +0300

    c2

---
+++ src/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+  const links = ['Home', 'About', 'Link'];
+</script>
+
+<main>
+  <nav>
+    <a href="#!">Home</a>
+    <a href="#!">About</a>
+    <a href="#!">Link</a>
+  </nav>
+</main>
+
+<style>
+  nav {
+    display: inline-block;
+  }
+</style>
