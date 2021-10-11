80292d67388fac0875ab67a65787b2215502d9a3
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-10-11 12:55:20 +0300

    c3

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,22 @@
 <script>
   import Header from './Header.svelte';
-  export let name = 'world';
+  let name = 'world';
+
+  function setName({ target }) {
+    const { value } = target;
+
+    name = value || 'world';
+
+    console.log(value);
+  }
 </script>
 
 <main>
   <Header />
-
-  <h1>Hello {name}!</h1>
+  <div>
+    <input type="text" placeholder="Enter your name" on:keyup={setName} />
+    <h1>Hello {name}!</h1>
+  </div>
 </main>
 
 <style>
@@ -15,5 +25,9 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
+  }
+
+  div {
+    margin-top: 25px;
   }
 </style>
